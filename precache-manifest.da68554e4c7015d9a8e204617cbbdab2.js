425b49163ed0f39ec478c5207f8c82c9fc39e242
Akbarbar30 <mohammadakbar28@yahoo.co.id>
2020-10-14 10:39:47 +0700

    Updates

---
+++ precache-manifest.da68554e4c7015d9a8e204617cbbdab2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65619c906c4b1db3901652cd25bd9c5c",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "b268d018daaf12cca777",
+    "url": "/movie-search/static/css/main.fd76877b.chunk.css"
+  },
+  {
+    "revision": "642b2585732676e88758",
+    "url": "/movie-search/static/js/2.afeff748.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/movie-search/static/js/2.afeff748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b268d018daaf12cca777",
+    "url": "/movie-search/static/js/main.25ebb6ca.chunk.js"
+  },
+  {
+    "revision": "8f1314dfdc21c95c9344",
+    "url": "/movie-search/static/js/runtime-main.8345f5b5.js"
+  }
+]);
